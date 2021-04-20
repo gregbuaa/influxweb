de04fb7c36beef44b43d03057bfa739e119b7981
zhang <zhangnet@buaa.edu.cn>
2021-04-19 23:22:05 -0400

    修改influx-proxy的recovery功能

--- management/views.py
+++ management/views.py
@@ -23,13 +23,13 @@ def influx_manage(request, info):
         contents =  influx_current_state(request)
         html_file = "current_state.html"
     elif info == "influx_sync":
-        contents = influx_sync(request)
+        contents = influx_return(request, "resync")
         html_file = "manage_synr.html"
     elif info == "influx_rebalance":
-        contents = influx_rebalance(request)
+        contents = influx_return(request, "rebalance")
         html_file = "manage_rebalance.html"
     elif info == "influx_recovery":
-        contents = influx_recovery(request)
+        contents = influx_return(request, "recovery")
         html_file = "manage_recovery.html"
 
 
@@ -41,15 +41,14 @@ def json_manage(request,info):
         contents =  influx_current_state(request)
         html_file = "current_state.html"
     elif info == "influx_sync":
-        contents = influx_sync(request)
+        contents = influx_return(request, "sync")
         html_file = "manage_synr.html"
     elif info == "influx_rebalance":
-        contents = influx_rebalance(request)
+        contents = influx_return(request, "rebalance")
         html_file = "manage_rebalance.html"
     elif info == "influx_recovery":
-        contents = influx_recovery(request)
+        contents = influx_return(request, "recovery")
         html_file = "manage_recovery.html"
-
 
     return JsonResponse(contents,safe=False)
 
@@ -95,88 +94,37 @@ def influx_current_state(request):
         "health_state":health_states
     }
     
-
     return  contents
 
 
-def influx_sync(request):
+INFLUX_PROXY_CONFIG_PATH = r"/home/zeal/project/influx-proxy/"
+def influx_return(request, order):
     influx_list = get_all_influx_site()
-
-    site_list = []
-
     http = urllib3.PoolManager()
     max_cicrle = 10
     sync_info = []
-    for data in influx_list:
-        r = http.request('GET', 'http://%s:%s/transfer/state?u=%s&p=%s'%(data["ip"],data["port"],data["user"],data["passwd"])) ## ip后面会从MySQL配置表中取出来！
-        states = json.loads(r.data.decode('utf-8'))
-        site_list.append({
-                "site_no":str(data["site_no"]),
-                "site_name":data["site_name"],
-                "chinese_name":data["chinese_name"],
-                "states":str(states["resyncing"])
-            }
-        )
-        for i in range(max_cicrle):
-            r = http.request('GET', 'http://%s:%s/transfer/stats?circle_id=%d&type=resync&u=%s&p=%s'%(data["ip"],data["port"],i,data["user"],data["passwd"])) ## ip后面会从MySQL配置表中取出来！
-            circle = json.loads(r.data.decode('utf-8'))
-            if "error" in circle:
-                break
-            circle["site_no"] = "站点"+data["chinese_name"]+ data["site_name"] + "的第"+str(i+1)+"个cicrle"
-            sync_info.append(circle)
+    site_list = []
 
     
-    has_sync = {}
-    for data in sync_info:
-        for urlname, meta in data.items():
-            if "http" not in urlname:
-                continue
-            if meta['database_total'] != meta['database_done']:
-                has_sync[urlname] = False
-                continue
-            if meta['measurement_total'] != meta['measurement_done']:
-                has_sync[urlname] = False
-                continue
-
-            has_sync[urlname] = True
-
-
-    contents = {
-        "site_list":site_list,
-        "sync_state": sync_info,
-        "has_sync":has_sync
-    }
-
-    print(contents)
-
-    # return render(request,'manage_synr.html',contents)
-    return contents
-
-
-INFLUX_PROXY_CONFIG_PATH = r"/home/zeal/project/influx-proxy/"
-
-
-def influx_rebalance(request):
-    influx_list = get_all_influx_site()
-    http = urllib3.PoolManager()
-    max_cicrle = 10
-    sync_info = []
-    site_list = []
     for data in influx_list:
-        with open(INFLUX_PROXY_CONFIG_PATH + data["config_file"],"r") as f:
-            proxy_config = json.load(f)
-            for circle in proxy_config["circles"]:
-                for backend in circle['backends']:
-                    backend["auth_secure"] = str(backend["auth_secure"])
-            # print(proxy_config)
-
         r = http.request('GET', 'http://%s:%s/transfer/state?u=%s&p=%s'%(data["ip"],data["port"],data["user"],data["passwd"])) ## ip后面会从MySQL配置表中取出来！
         states = json.loads(r.data.decode('utf-8'))
-        transferring = False
-        for element in states["circles"]:
-            if element["transferring"]:
-                transferring = True
-                break
+
+        if order == "rebalance" or order == "recovery": 
+            with open(INFLUX_PROXY_CONFIG_PATH + data["config_file"],"r") as f:
+                proxy_config = json.load(f)
+                for circle in proxy_config["circles"]:
+                    for backend in circle['backends']:
+                        backend["auth_secure"] = str(backend["auth_secure"])
+            transferring = "False"
+            for element in states["circles"]:
+                if element["transferring"]:
+                    transferring = "True"
+                    break
+        else:
+            proxy_config = {}
+            transferring = str(states["resyncing"])
+
 
         site_list.append({
                 "site_no":str(data["site_no"]),
@@ -187,7 +135,7 @@ def influx_rebalance(request):
             }
         )
         for i in range(max_cicrle):
-            r = http.request('GET', 'http://%s:%s/transfer/stats?circle_id=%d&type=rebalance&u=%s&p=%s'%(data["ip"],data["port"],i,data["user"],data["passwd"])) ## ip后面会从MySQL配置表中取出来！
+            r = http.request('GET', 'http://%s:%s/transfer/stats?circle_id=%d&u=%s&p=%s&type=%s'%(data["ip"],data["port"],i,data["user"],data["passwd"],order)) ## ip后面会从MySQL配置表中取出来！
             circle = json.loads(r.data.decode('utf-8'))
             if "error" in circle:
                 break
@@ -208,8 +156,6 @@ def influx_rebalance(request):
 
             has_sync[urlname] = True
 
-
-
     contents = {
         "site_list":site_list,
         "sync_state": sync_info,
@@ -217,108 +163,6 @@ def influx_rebalance(request):
     }
 
     return contents
-
-
-def influx_recovery(request):
-
-    proxy_config = '''{
-    "circles": [
-        {
-            "name": "circle-1",
-            "backends": [
-                {
-                    "name": "influxdb-1-1",
-                    "url": "http://127.0.0.1:14001",
-                    "username": "",
-                    "password": "",
-                    "auth_secure": false
-                },
-                {
-                    "name": "influxdb-1-2",
-                    "url": "http://127.0.0.1:14002",
-                    "username": "",
-                    "password": "",
-                    "auth_secure": false
-                }
-            ]
-        },
-        {
-            "name": "circle-2",
-            "backends": [
-                {
-                    "name": "influxdb-2-1",
-                    "url": "http://127.0.0.1:14003",
-                    "username": "",
-                    "password": "",
-                    "auth_secure": false
-                },
-                {
-                    "name": "influxdb-2-2",
-                    "url": "http://127.0.0.1:14004",
-                    "username": "",
-                    "password": "",
-                    "auth_secure": false
-                }
-            ]
-        }
-    ],
-    "listen_addr": ":7920",
-    "db_list": [],
-    "data_dir": "data",
-    "tlog_dir": "log",
-    "hash_key": "idx",
-    "flush_size": 10000,
-    "flush_time": 1,
-    "check_interval": 1,
-    "rewrite_interval": 10,
-    "conn_pool_size": 20,
-    "write_timeout": 10,
-    "idle_timeout": 10,
-    "username": "",
-    "password": "",
-    "auth_secure": false,
-    "write_tracing": false,
-    "query_tracing": false,
-    "https_enabled": false,
-    "https_cert": "",
-    "https_key": ""
-    }
-    '''
-
-    http = urllib3.PoolManager()
-    max_cicrle = 10
-
-    sync_info = []
-    for i in range(max_cicrle):
-        r = http.request('GET', 'http://192.168.231.200:7920/transfer/stats?circle_id=%d&type=recovery'%(i)) ## ip后面会从MySQL配置表中取出来！
-        circle = json.loads(r.data.decode('utf-8'))
-        if "error" in circle:
-            break
-        sync_info.append(circle)
-
-    
-    has_sync = {}
-
-
-    for data in sync_info:
-        for urlname, meta in data.items():
-            if meta['database_total'] != meta['database_done']:
-                has_sync[urlname] = False
-                continue
-            if meta['measurement_total'] != meta['measurement_done']:
-                has_sync[urlname] = False
-                continue
-
-            has_sync[urlname] = True
-
-    contents = {
-        "sync_state": sync_info,
-        "has_sync":has_sync,
-        "proxy_config":json.loads(proxy_config)
-    }
-
-    return contents
-
 
 def data_sync(request):
     tick = request.GET.get('from_timestap', '0')
@@ -360,7 +204,6 @@ def data_rebalance(request):
     
     operator_symbol = "add" 
 
-    print(table_content)
     for data in table_content:
         if "checkbox" in data and data["checkbox"]==True:
             operator_symbol = "rm"
@@ -417,58 +260,74 @@ def data_rebalance(request):
 
 
 def data_recovery(request):
-
-    to_circle = request.POST.get('to_circle', 'circle-1')
-    from_circle = request.POST.get('from_circle', 'circle-1')
+    to_circle = request.POST.get('to_circle', 'circle_1')
+    from_circle = request.POST.get('from_circle', 'circle_2')
     worker_num = request.POST.get('worker_num', '4')
     recovery_influx = request.POST.get('recovery_influx', '')
     table_content = request.POST.get('table_content', "[]")
+    site_no = request.POST.get("site_no","1")
 
-    table_json = json.loads(table_content)
-    new_table_json = []
-    for item in table_json:
-        if 'checkbox' in item and item['checkbox']==True:
-            ## 删除的坏的influxdb实例，从proxy.json文件中拿掉
-            continue
-        if 'checkbox' in item:
-            del item['checkbox']
-        new_table_json.append(item)
-
-    ##恢复代码
-    ## 根据table_content修改proxy.json
-    PROXY_DIR = ""
-    reader = open(PROXY_DIR, 'r')
-    orignal_proxy = json.load(reader)
-    
-    new_circles = []
-    to_circle_id = -1
-    from_circle_id = -1
-    for index, circle in enumerate(orignal_proxy['circles']):
-        if circle['name'] == to_circle:
-            new_circles.append({
-                'name':to_circle,
-                'backends': new_table_json
-            })
-            to_circle_id = index
-            continue
-        if circle['name'] == from_circle:
-            from_circle_id = index
-        
-        new_circles.append(circle)
-
-    orignal_proxy['circles'] = new_circles
-    ## 重启所有的influx proxy
-
-
-    ## 根据接口/recovery进行操作 
+    table_content = json.loads(table_content)
     http = urllib3.PoolManager()
-    r = http.request('POST','http://192.168.231.200:7920/recovery?from_circle_id=%d&to_circle_id=%d&worker=%s&backend_urls=%s'%(from_circle_id,to_circle_id,worker_num,recovery_influx))
-    response_state = r.data.decode('utf-8')
 
-    # response_state = "accepted"
+
+    print(to_circle, from_circle, table_content)
+
+
+    # rebalance代码
+    ## 修改proxy.json实例，增删实例
+    site_info = Influxsite.objects.get(site_no = int(site_no))
+
+    with open(INFLUX_PROXY_CONFIG_PATH + site_info.config_file,"r") as f:
+        proxy_config = json.load(f)
+
+    new_circle_config = []
+
+    print(table_content)
+    for data in table_content:
+        if "checkbox" in data and data["checkbox"]==True:
+            continue
+        new_circle_config.append({
+            "name":data["name"],
+            "url":data["url"],
+            "username":data["username"],
+            "password":data["password"],
+            "auth_secure": False if data['auth_secure'] == "False" else True
+        })
+
+    to_circle_id = 0
+    from_circle_id = 0
+    for count, circle in enumerate(proxy_config["circles"]):
+        if circle["name"] == to_circle:
+            circle["backends"] = new_circle_config
+            to_circle_id = count
+        if circle["name"] == from_circle:
+            from_circle_id = count
+
+    ## 重启influx-proxy代理
+    proxy_text = json.dumps(proxy_config,indent=4)
+    print(proxy_text)
+
+    with open(INFLUX_PROXY_CONFIG_PATH + site_info.config_file,"w") as f:
+        f.write(proxy_text)
+    ## 调用这个命令来做
+    # ps -ef|grep hello|grep -v grep|cut -c 9-15 | xargs kill -9
+    rc = subprocess.check_call(r'ps -ef|grep %s|grep -v grep|cut -c 9-15 | xargs kill -9'%(site_info.config_file), shell=True)
+    print("rc",rc)
+    os.chdir(INFLUX_PROXY_CONFIG_PATH)
+    rc2 =  subprocess.check_call(r'nohup ./influx-proxy -config %s &'%(site_info.config_file), shell=True)
+    print("rc2",rc2)
+
+    time.sleep(2)
+
+    r =  http.request('POST','http://%s:%s/recovery?from_circle_id=%d&to_circle_id=%d&worker=%s&u=%s&p=%s'%(site_info.ip, str(site_info.port),from_circle_id,to_circle_id,worker_num,site_info.user,site_info.passwd))
+    response_state = r.data.decode('utf-8')
+    ## 状态查询
     content= {
         "response_state":response_state
     }
+
+    print("恢复命令已经发出！！！")
     return JsonResponse(content,safe=False)
 
 
